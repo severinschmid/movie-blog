98e3f7b0b8e526989c6897ca6018911928bd1813
Severin Schmid <severin.schmid@sz.de>
2020-12-29 20:29:45 +0100

    Add more content

--- routes/movies.js
+++ routes/movies.js
@@ -11,18 +11,49 @@ const movies = [
     description: "Another Jake Gyllenhaal favorite. It is pretty great, I think even one of my favorites. It´s dark but nothing you can´t handle. This was also the first time I noticed Riz Ahmed and since then I kind of follow his career, too. He is also in Una, The Sisters Brothers and Sound of Metal"
   },
   {
+    name: "Super Dark Times",
+    platform: "Netflix",
+    description: "Pretty intense thriller. It is so low key, but it is riveting and sometimes infuriating. I remember it to look awesome, too."
+  },
+  {
+    name: "Marriage Story",
+    platform: "Netflix",
+    description: "You should know this one by now. It starrs Adam Driver so of course it is going to appear here. Noah Baumbach made movies in the past that made me think of him as pretentious, but not this one. It is very good."
+  },
+  {
+    name: "I am thinking of ending things",
+    platform: "Netflix",
+    description: "A weird one which is kind of hard to recommend. But if you feel that you have seen everything, go see this. I think I have to see this more times to completely get it, or get it at all."
+  },
+  {
+    name: "It comes at night",
+    platform: "Netflix",
+    description: "I can´t keep saying that movies are thrilling. I love A24, and the majority of movies they put out, this one included. The atmosphere is this is extremely well done, and the posters were amazing, too. The trailer makes it seem more like a horror movie than it acutally is, so give it chance."
+  },
+  {
     name: "Burning",
     platform: "Amazon",
-    description: "I guess this is the movie I think about the most, it is currently in my top three favorite movies. To be brief, I was just so immersed into this. The world feels so lived-in, the performances are great, and it is one of the best looking films I know. No other movie I know depicts poverty and class in South Korea like Burning."
+    description: "I guess this is the movie I think about the most, it is currently in my top three favorite movies. To be brief, I was just so immersed into this. The world feels so lived-in, the performances are great, and it is one of the best looking films I have ever seen. No other movie I know depicts poverty and class in South Korea like Burning. Some day I am going to make all my friends watch it."
   },
   {
     name: "Prisoners",
     platform: "Netflix",
     description: "My favorite movie for a long time. I guess when i first saw this, no other movie has been this thrilling. I was on the edge of my seat the entire time. My love for Denis Villeneuve really started with this one."
-  }, {
+  },
+  {
     name: "Arrival",
     platform: "Amazon",
     description: "You have to see this because it is from Denis Villeneuve. One of the best science fiction movies because it seems so real. The score is absolutely beautiful. Sadly, the composer Johann Johannsson died in 2018."
+  },
+  {
+    name: "American Factory",
+    platform: "Netflix",
+    description: "Produced by the Obamas, this documentary won the academy award. Maybe you think you don´t care about the subject matter but you will still enjoy this."
+  },
+  {
+    name: "Okja",
+    platform: "Netflix",
+    description: "You should see any movie by Bong Joon-Ho that you can find. Please still watch them in the original with subtitles. This one is on Netflix and large part of it are in engish so it is the perfect gateway."
   }
 ]
 
--- routes/shows.js
+++ routes/shows.js
@@ -8,7 +8,7 @@ const shows = [
   {
     name: "Queens Gambit",
     platform: "Netflix",
-    description: "It is just nice to watch."
+    description: "This is pretty entertaining and low maintenance. No commitment, just a couple of episodes, in fact it started out to become a movie."
   },
   {
     name: "Ozark",
@@ -18,12 +18,12 @@ const shows = [
   {
     name: "Breaking Bad",
     platform: "Netflix",
-    description: "One of my all-time favorites and a must-watch."
+    description: "One of my all-time favorites and a must-watch. Just insanely well made and exciting."
   },
   {
     name: "Better Call Saul",
     platform: "Netflix",
-    description: "I started watching this after 3 seasons or so have been released. I was skeptical because it is a spin-off, but it works on its own extremely well. I could go on about this, but this is one of those where I could not believe how entertaining it is to watch. Like Barry."
+    description: "I started watching this after 3 seasons or so have been released. I was skeptical because it is a spin-off of Breaking Bad, but it works on its own extremely well. I could go on about this, but this is one of those where I could not believe how entertaining it is to watch. Like Barry."
   },
   {
     name: "Barry",
@@ -34,6 +34,16 @@ const shows = [
     name: "Devs",
     platform: "Sky\xa0Ticket",
     description: "From one of my favorite film makers, Alex Garland. So far he made on hit after the other as a director. His two movies are Ex Machina and Annihilation. This is a limited series and very engaging. It is science fiction but I still would recommend this to everyone."
+  },
+  {
+    name: "Succession",
+    platform: "Sky\xa0Ticket",
+    description: "A pretty recent show, it is just in its second season and picking up emmys left and right. The family is awful, but that is the entire point. It is so funny and satirical and features dialogues which remind me a lot of Mad Men. The money stuff in it seems to be very grounded in reality as well, Slate Money even made a podcast about Succession storylines and its real world counterparts."
+  },
+  {
+    name: "Big Little Lies",
+    platform: "Sky\xa0Ticket",
+    description: "This one is for all the moms. I liked it too though, more so the first season. Still, this is prestige TV with an all-star cast. All the kid actors are great, and its pretty funny at times."
   },
   {
     name: "Catastrophe",
